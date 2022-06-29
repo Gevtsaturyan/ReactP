53f9237dfc93ffde8b7ecb03ca5d4b6598d81f1a
Gevtsaturyan <gevtsatur1996@gmail.com>
2022-06-29 11:29:56 +0400

    contact

--- src/redux/rootReduser.js
+++ src/redux/rootReduser.js
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
 //sranov menq miacnum enq bolor redusernery mer komponentnerin
 import { likesReduser } from "./about/reduser";
+import contactReducer from "./contact/contact.reduser";
+
 
 
 
 
 const rootReduser=combineReducers({ //stexel grelu enq bolor redusernery
+    contact:contactReducer,
     likes:likesReduser,
     text:likesReduser,
     comments:likesReduser,
